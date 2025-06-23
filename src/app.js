d8a8b80d8ed0d40d671a780775742a23da847b17
OsorioDanny <daosva97@gmail.com>
2025-06-23 19:51:50 +0000

    finish

--- src/app.js
+++ src/app.js
@@ -7,5 +7,38 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extensiones = ['.com', '.net', '.us', '.io'];
+
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let a = 0; a < adj.length; a++) {
+    for (let b = 0; b < noun.length; b++) {
+      console.log(pronoun[i] + adj[a] + noun[b]);
+
+    }
+
+  };
+
+}
+
+
+
+//Agrega varios tipos de extensiones, por ejemplo: .com, .net, .us, .io, etc.
+
+pronoun.map((pronoun) => {
+  adj.map((adj) => {
+    noun.map((noun) => {
+      extensiones.map((extensiones) => {
+        console.log(pronoun + adj + noun + extensiones);
+
+      });
+    });
+  });
+});
+
 };
